8b5c952a8b3ce250672a6bd6f3b61beec2d4598c
Trish Burns <pb12ow@brocku.ca>
2021-12-09 21:11:52 -0500

    Create lec8Exercise2.js

---
+++ lec8/lec8Exercise2.js
@@ -0,0 +1,17 @@
+//Sets the initial colours of the three elements on page load
+
+
+//A function to change colours of elements when called
+function mix(){
+	document.getElementById("blue").style.color="blue";
+	document.getElementById("green").style.color="green";
+	document.getElementById("red").style.color="red";
+}
+
+	var first =  "#00BB00";
+	var second = "BB9320";
+	var third = "#AB00FF";
+
+	first= document.getElementById("blue").style.color="first";
+	second= document.getElementById("green").style.color="second";
+	third= document.getElementById("red").style.color="third";
